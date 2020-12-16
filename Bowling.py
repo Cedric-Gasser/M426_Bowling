4f2156268da35699cab4c1d01d7d5835c62d4b7c
Cedric Gasser <cedric_gasser@sluz.ch>
2020-12-16 12:23:18 +0100

    coded the stuff

--- Bowling.py
+++ Bowling.py
@@ -1,6 +1,5 @@
 result = 0
 
-
 def score(scoreString):
     global result
     result = _convertScore(scoreString)
@@ -11,4 +10,43 @@ def getResult() -> int:
 
 
 def _convertScore(scoreString) -> int:
-    return 300
+    scoreString = scoreString.upper();
+    scorePerRound = []
+
+    pointer = 0
+    for i in range(10):
+        round = scoreString[pointer]
+        if round == "X":
+            scorePerRound.append(scoreString[pointer] + scoreString[pointer+1] + scoreString[pointer+2])
+            pointer += 1
+
+        elif int(round) < 10 and int(round) > 0 and scoreString[pointer + 1] == "/":
+            scorePerRound.append(scoreString[pointer] + scoreString[pointer+1] + scoreString[pointer+2])
+            pointer += 2
+
+        else:
+            scorePerRound.append(scoreString[pointer] + scoreString[pointer + 1])
+            pointer += 2
+
+    print(scorePerRound)
+
+    res = 0
+    for i in scorePerRound:
+        res += parseCode(i)
+
+    return res
+
+def parseCode(roundString) -> int:
+    res = 0
+    for i in range(len(roundString)):
+        if roundString[i] == "X":
+            res += 10
+        elif roundString[i] == "/":
+            res += 10
+            res -= int(roundString[i-1])
+        else:
+            res += int(roundString[i])
+    return res
+
+score = input()
+print(_convertScore(score))
