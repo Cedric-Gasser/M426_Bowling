49065b5cfa9920cdbdf6ea3e805c2e4370189aea
Cedric Gasser <cedric_gasser@sluz.ch>
2020-12-16 22:44:24 +0100

    Implemented Errorhandling for too many pins and added more tests

--- Bowling.py
+++ Bowling.py
@@ -1,3 +1,5 @@
+import re
+
 result = 0
 
 
@@ -11,21 +13,23 @@ def getResult() -> int:
 
 
 def convertScore(score_string) -> int:
-    score_string = score_string.upper()
+    score_string = score_string.upper().replace("-", "0")
     score_per_round = []
 
     pointer = 0
     for i in range(10):
-        if score_string[pointer] == "X":
+        if score_string[pointer] == "X":  #When there's a strike
             score_per_round.append(score_string[pointer] + score_string[pointer + 1] + score_string[pointer + 2])
             pointer += 1
 
-        elif 10 > int(score_string[pointer]) > 0 and score_string[pointer + 1] == "/":
+        elif score_string[pointer + 1] == "/":   #When there's a spare
             score_per_round.append(score_string[pointer] + score_string[pointer + 1] + score_string[pointer + 2])
             pointer += 2
 
-        else:
+        else:    #When there are no special cases
             score_per_round.append(score_string[pointer] + score_string[pointer + 1])
+            if int(score_string[pointer]) + int(score_string[pointer + 1]) > 9:
+                raise Exception("More than 10 pins?!")
             pointer += 2
 
     print(score_per_round)
@@ -45,8 +49,6 @@ def parseCode(round_string) -> int:
         elif round_string[i] == "/":
             res += 10
             res -= int(round_string[i - 1])
-        elif round_string[i] == "-":
-            round_string[i] = "0"
         else:
             res += int(round_string[i])
     return res
--- BowlingTest.py
+++ BowlingTest.py
@@ -1,11 +1,28 @@
 import unittest
 import Bowling
 
+
 class BowlingTest(unittest.TestCase):
     def test_allStrikes(self):
         Bowling.score("XXXXXXXXXXXX")
         result = Bowling.getResult()
-        self.assertEqual(result, 300)
+        self.assertEqual(300, result)
+
+    def test_allSpares(self):
+        Bowling.score("3/4/9/2/9/0/-/-/1/6/8")
+        result = Bowling.getResult()
+        self.assertEqual(139, result)
+
+    def test_hittinNothingAtAll(self):
+        Bowling.score("--00--00--00--00--00")
+        result = Bowling.getResult()
+        self.assertEqual(0, result)
+
+    def testException_moreThan10Pins(self):
+        with self.assertRaises(Exception) as ex:
+            Bowling.score("99999999999999999999")
+        the_exception = ex.exception
+        self.assertEqual(str(Exception("More than 10 pins?!")), str(the_exception))
 
 
 if __name__ == '__main__':
